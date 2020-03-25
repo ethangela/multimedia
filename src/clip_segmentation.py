e17d0d4b4da5ab679053bf9604a3a921bbe33937
JKOK005 <JKOK005@e.ntu.edu.sg>
2020-03-26 00:53:34 +0800

    Partial implementation of clip_segmentation.py

--- src/clip_segmentation.py
+++ src/clip_segmentation.py
@@ -8,8 +8,10 @@ import pandas as pd
 from editing.segmentation import VideoSegmentation
 from editing.writer import DatasetWriter
 
-CLIP_DURATION = int(os.environ.get("CLIP_DURATION", 4))
-VIDEO_METADATA_PATH = os.environ["METADATA_PATH"]
+CLIP_DURATION 			= int(os.environ.get("CLIP_DURATION", 4))
+UNEDITED_CLIPS_ROOT 	= os.environ["CLIPS_ROOT"] 		
+SEGMENTED_CLIPS_ROOT 	= os.environ["SEGMENT_ROOT"] 	
+VIDEO_METADATA_PATH 	= os.environ["METADATA_PATH"]
 
 logging.basicConfig(filename='clip_segmentation.log',level=logging.DEBUG)
 
