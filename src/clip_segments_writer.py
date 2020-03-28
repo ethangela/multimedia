137db0559eb2d811cd26006b37500f4d8265b8bd
johan.kok <johan.kok@grabtaxi.com>
2020-03-28 14:44:17 +0800

    Refractored clip segment writer

--- src/clip_segments_writer.py
+++ src/clip_segments_writer.py
@@ -1,6 +1,12 @@
 import os
 import glob
 import logging
+import multiprocessing
+import numpy as np
+import pandas as pd
+from commons.executions import multiple_executions_wrapper
+from editing.segmentation import VideoSegmentation
+from editing.writer import DatasetWriter
 
 MAX_THREAD_POOL 		= int(os.environ.get("THREAD_POOL", multiprocessing.cpu_count()))
 SEGMENTED_CLIPS_ROOT 	= os.environ["SEGMENTED_CLIPS_ROOT"] 	# Write out to root dir here
@@ -8,6 +14,10 @@ SEGMENTED_CLIPS_PATH 	= os.environ.get("SEGMENTED_CLIPS_PATH", "_splits")
 
 logging.basicConfig(level=logging.INFO)
 
+segmenter_obj 	= VideoSegmentation()
+data_writer 	= DatasetWriter()
+
+@multiple_executions_wrapper
 def write_segment_clip(segment_metadata_df: pd.DataFrame) -> None:
 	for _, row in segment_metadata_df.iterrows():
 		full_video 	= segmenter_obj.readAsVideo(video_path = row["full_video_path"])
@@ -16,13 +26,14 @@ def write_segment_clip(segment_metadata_df: pd.DataFrame) -> None:
 		logging.info("PID {0} - Write to {1}".format(os.getpid(), row["segmented_clips_path"]))
 	return
 
-def exec_write_segment_clip(path_to_df: str) -> None:
-	segment_metadata_df = pd.read_csv(path_to_df)
+def exec_write_segment_clip(segment_metadata_df: pd.DataFrame) -> None:
 	write_segment_clip(segment_metadata_df = segment_metadata_df)
 	return
 
 if __name__ == "__main__":
 	# Begin writing segments to dfs
-	segment_metadata_files = glob.glob(os.path.join(SEGMENTED_CLIPS_ROOT, SEGMENTED_CLIPS_PATH) + "/*segment_metadata.csv")
+	segment_metadata_files 		= glob.glob(os.path.join(SEGMENTED_CLIPS_ROOT, SEGMENTED_CLIPS_PATH) + "/*_segment_metadata.csv")
+	segment_metadata_df 		= pd.concat(map(pd.read_csv, segment_metadata_files))
+	segment_metadata_df_splits 	= np.array_split(segment_metadata_df, MAX_THREAD_POOL)
 	with multiprocessing.Pool(processes = MAX_THREAD_POOL) as pool:
-		pool.map(exec_write_segment_clip, segment_metadata_files)
+		pool.map(exec_write_segment_clip, segment_metadata_df_splits)
