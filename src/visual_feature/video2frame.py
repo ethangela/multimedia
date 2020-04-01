939758061f358dc7d171832bd3112c88edee7461
johan.kok <johan.kok@grabtaxi.com>
2020-04-01 23:56:43 +0800

    Some code refractoring - Define directories by environment variables

--- src/visual_feature/video2frame.py
+++ src/visual_feature/video2frame.py
@@ -3,36 +3,56 @@ import cv2
 import numpy as np
 import os
 
+SEGMENTED_CLIPS_ROOT    = os.environ["SEGMENTED_CLIPS_ROOT"]
+IMAGE_ROOT              = os.environ["IMAGE_ROOT"]
+SAMPLE_FPS              = int(os.environ.get("SAMPLE_FPS", -1))
+
 def video2frame_at2FPS(name):
-    add = './videos/' + name
-    cap = cv2.VideoCapture(add)
+    cap = cv2.VideoCapture(name)
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     duration = frame_count/fps
     
-    output_dir = './frames/' + str(name[:-4])
+    video_name  = name.split("/")[-1].replace(".mp4", "")
+    output_dir  = os.path.join(IMAGE_ROOT, video_name.replace(".mp4", "")) 
     try:
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
     except OSError:
         print ('Error')
-        
-    currentFrame = 0.0 #if FPS changes then the corresponding count number shall also change 
-    while currentFrame < duration:
-        cap.set(cv2.CAP_PROP_POS_MSEC, (currentFrame*500)) #currentFrame*500 == FPS@2, currentFrame*1000 == FPS@1, ... etc.
-        ret, frame = cap.read()
-        
-        title = output_dir + '/' + '_frame' + str(int(currentFrame*2)) + '.jpg' # #if FPS changes then the corresponding count number shall also change
-        print('creating... '+ title)
-        cv2.imwrite(title, frame)
-        
-        currentFrame += 0.5 #if FPS changes then the corresponding count number shall also change
+    
+    if SAMPLE_FPS > 0:
+        currentFrame = 0.0 #if FPS changes then the corresponding count number shall also change 
+        while currentFrame < duration:
+            cap.set(cv2.CAP_PROP_POS_MSEC, (currentFrame*500)) #currentFrame*500 == FPS@2, currentFrame*1000 == FPS@1, ... etc.
+            ret, frame = cap.read()
+            
+            title   = os.path.join(output_dir, "{0}_fps_{1}.jpg".format(video_name, str(currentFrame*2)))
+            print('creating... '+ title)
+            cv2.imwrite(title, frame)
+            
+            currentFrame += 0.5 #if FPS changes then the corresponding count number shall also change
+    else:
+        # Do not subsample by FPS
+        success     = cap.grab()
+        frame_indx  = 0
+        while success:
+            _, frame = cap.retrieve()
+            
+            title   = os.path.join(output_dir, "{0}_fps_{1}.jpg".format(video_name, frame_indx))
+            print('creating... '+ title)
+            cv2.imwrite(title, frame)
+            frame_indx += 1
+            success = cap.grab()   
         
     cap.release()
     cv2.destroyAllWindows()
     
 if __name__ == '__main__':
-    base_dir = './videos'
-    v_list = os.listdir(base_dir)
-    for item in v_list:
-        video2frame_at2FPS(item)
+    base_dir = '/Users/johan.kok/Downloads/multimedia/subclips/Belly_dance/'
+
+    for directory, sub_dir, files in os.walk(SEGMENTED_CLIPS_ROOT):
+        for each_file in files:
+            if ".MP4" in each_file.upper():
+                video_path = os.path.join(directory, each_file)
+                video2frame_at2FPS(name = video_path)
