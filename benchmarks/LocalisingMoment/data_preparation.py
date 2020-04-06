744cfb9037540843e7cf59c5496f2e6faa438f45
johan.kok <johan.kok@grabtaxi.com>
2020-04-07 01:17:36 +0800

    bug fix

--- benchmarks/LocalisingMoment/data_preparation.py
+++ benchmarks/LocalisingMoment/data_preparation.py
@@ -89,7 +89,7 @@ def data_preprocessing(features: h5py._hl.files.File, segment_df: pd.DataFrame) 
 def execute(segment_df: pd.DataFrame) -> None:
 	image_features 	= h5py.File(FEATURES_FILE, "r")
 	df = data_preprocessing(features = image_features, segment_df = segment_df)
-	output_file = os.path.join(SEGMENTED_CLIPS_ROOT, SEGMENTED_CLIPS_PATH, "data.csv")
+	output_file = os.path.join(SEGMENTED_CLIPS_ROOT, SEGMENTED_CLIPS_PATH, "{0}_data.csv".format(os.getpid()))
 	data_writer.writeCsv(df = df, location = output_file)
 
 if __name__ == "__main__":
