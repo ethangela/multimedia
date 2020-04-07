ee4f4ef8800f4ad001f4155aac03b4fd16c6b4f7
johan.kok <johan.kok@grabtaxi.com>
2020-04-07 23:08:03 +0800

    bug fix

--- src/editing/writer.py
+++ src/editing/writer.py
@@ -27,6 +27,6 @@ class DatasetWriter(object):
 
 	def writeHdf5(self, df: pd.DataFrame, location: str) -> None:
 		self._createParents(path = location)
-		with pd.HDFStore(location, mode="w") as f:
-			f.append(key="/dataset", value=df, format="table", data_columns=df.columns)
-		# df.to_hdf(location, key='/dataset', mode='w')
+		# with pd.HDFStore(location, mode="w") as f:
+		# 	f.append(key="/dataset", value=df, format="table", data_columns=df.columns)
+		df.to_hdf(location, key='/dataset', mode='a', table=True)
