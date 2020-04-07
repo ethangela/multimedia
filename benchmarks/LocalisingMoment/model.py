24b33c7471e9df05ac6cc04c8b35b045563f7f83
johan.kok <johan.kok@grabtaxi.com>
2020-04-08 01:53:36 +0800

    bug fix

--- benchmarks/LocalisingMoment/model.py
+++ benchmarks/LocalisingMoment/model.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import logging
 import os
 from tensorflow.keras import layers, Model, metrics
-from tensorflow.keras.layers import LSTM, Dense, Input, concatenate, Flatten
+from tensorflow.keras.layers import LSTM, Dense, Input, concatenate, Flatten, Subtract
 
 LSTM_NUM_TIMESTEPS 	= 15 	# Also max sentence length
 LSTM_INPUT_DIM 		= 768
@@ -13,6 +13,9 @@ LSTM_HIDDEN_UNITS 	= 128
 VID_GLOBAL_FEAT 	= 2048 	# Dimension size
 VID_LOCAL_FEAT 		= 2048
 VID_TEMPORAL_FEAT 	= 2
+
+DENSE_LAYER_1 		= 40
+DENSE_OUTPUT_FEAT 	= 20
 
 TRAINING_DATA_PATH 	= os.environ["TRAINING_DATA_PATH"]
 CHECKPOINT_PATH 	= os.environ["MODEL_CHECKPOINT_PATH"]
@@ -42,7 +45,7 @@ def data_generator(csv_dir: str):
 				current_rows = language_tf.shape[0]
 				language_tf = np.pad(language_tf, [(0, LSTM_NUM_TIMESTEPS - current_rows), (0, 0)], mode = 'constant', constant_values = 0)  
 
-			yield ((temporal_tf, global_tf, local_tf, language_tf), (np.zeros(1, dtype = np.float32)))
+			yield ((temporal_tf, global_tf, local_tf, language_tf), (np.zeros(DENSE_OUTPUT_FEAT, dtype = np.float32)))
 
 def get_model():
 	sentence_embedding_input 	= tf.keras.Input(shape = (LSTM_NUM_TIMESTEPS, LSTM_INPUT_DIM,), dtype = tf.float32)
@@ -51,21 +54,21 @@ def get_model():
 	video_temporal_features 	= tf.keras.Input(shape = (VID_TEMPORAL_FEAT,), dtype = tf.float32)
 
 	# Sentence network
-	lstm_1 = LSTM(LSTM_HIDDEN_UNITS, return_sequences=True, return_state=True)
+	lstm_1 = LSTM(LSTM_HIDDEN_UNITS, return_sequences = True, return_state = True)
 	seq_out, hidden_out, carry_out = lstm_1(sentence_embedding_input)
-	sentence_out 	= Dense(32, activation=tf.nn.softmax)(hidden_out)
+	sentence_out 	= Dense(DENSE_OUTPUT_FEAT, activation=tf.nn.softmax)(hidden_out)
 
 	# Video feature network
 	merged_features = tf.concat([video_global_features, video_local_features, video_temporal_features], axis = 1)
-	dense_1			= Dense(50, activation=tf.nn.softmax)(merged_features)
+	dense_1			= Dense(DENSE_LAYER_1, activation=tf.nn.softmax)(merged_features)
 	relu_1 			= tf.nn.relu(dense_1)
-	vid_feat_out 	= Dense(32, activation=tf.nn.softmax)(dense_1)
+	vid_feat_out 	= Dense(DENSE_OUTPUT_FEAT, activation=tf.nn.softmax)(dense_1)
 
 	# Loss computation
-	l2_norm_loss	 = tf.norm(vid_feat_out - sentence_out, ord='euclidean', axis = 1)
+	subtract_1 		= Subtract()([vid_feat_out, sentence_out])
 
-	model = Model(inputs = [video_temporal_features, video_global_features, video_local_features, sentence_embedding_input], 
-				  outputs = [l2_norm_loss])	
+	model = Model(inputs  = [video_temporal_features, video_global_features, video_local_features, sentence_embedding_input], 
+				  outputs = [subtract_1])	
 
 if __name__ == "__main__":
 	lm_net 	= get_model()
@@ -74,7 +77,7 @@ if __name__ == "__main__":
 
 	lm_net.compile(optimizer = tf.keras.optimizers.SGD(),
 	               loss = tf.keras.losses.MeanSquaredError(),
-	               metrics=[metrics.mean_squared_error])
+	               metrics = [metrics.mean_squared_error])
 
 	callbacks = [
 				    tf.keras.callbacks.ModelCheckpoint(
@@ -83,7 +86,6 @@ if __name__ == "__main__":
 				        monitor = "val_loss",
 				        verbose = 1)
 				]
-
 
 	history = model.fit(x = train_gen,
 						verbose = 1,
