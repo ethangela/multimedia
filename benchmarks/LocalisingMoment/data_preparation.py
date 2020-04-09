293d2cc9c2cc6d1511ed8e575deb2e8f9903296e
johan.kok <johan.kok@grabtaxi.com>
2020-04-09 18:44:52 +0800

    bug fix

--- benchmarks/LocalisingMoment/data_preparation.py
+++ benchmarks/LocalisingMoment/data_preparation.py
@@ -69,7 +69,7 @@ def get_local_encoding(features: h5py._hl.files.File, row) -> pd.Series:
 
 	feat_cp_wo_zeros 	= feat_cp[~np.all(feat_cp == 0, axis = 1)]
 	local_feat 			= np.mean(feat_cp_wo_zeros, axis = 0)
-	if local_feat is None:
+	if feat_cp_wo_zeros.size == 0:
 		print(feat_cp)
 	return local_feat.tolist()
 
