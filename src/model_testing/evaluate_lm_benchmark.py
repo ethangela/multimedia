cbc428163a02d9db1368deb8a4cb25b992a0b659
johan.kok <johan.kok@grabtaxi.com>
2020-04-11 17:49:05 +0800

    bug-fix

--- src/model_testing/evaluate_lm_benchmark.py
+++ src/model_testing/evaluate_lm_benchmark.py
@@ -194,4 +194,4 @@ if __name__ == "__main__":
 	acc_correct_selections 	= [i[0] for i in results]
 	acc_total_choices 		= [i[1] for i in results]
 
-	logging.intersection("Average percentage for R@{0} : {1} %".format(K_BEST, acc_correct_selections / acc_total_choices))
+	logging.info("Average percentage for R@{0} : {1} %".format(K_BEST, acc_correct_selections / acc_total_choices))
